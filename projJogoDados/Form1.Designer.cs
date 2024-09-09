93cba5eb6030636c8512ffe64a135b3e0b724740
MatheusCanesso <matheuscanesso562@gmail.com>
2024-09-09 15:14:51 -0300

    Adicionar arquivos de projeto.

---
+++ projJogoDados/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace projJogoDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSorteio = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(41, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 229);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSorteio
+            // 
+            this.btnSorteio.Location = new System.Drawing.Point(81, 281);
+            this.btnSorteio.Name = "btnSorteio";
+            this.btnSorteio.Size = new System.Drawing.Size(133, 38);
+            this.btnSorteio.TabIndex = 1;
+            this.btnSorteio.Text = "Sortear";
+            this.btnSorteio.UseVisualStyleBackColor = true;
+            this.btnSorteio.Click += new System.EventHandler(this.BtnSorteio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.btnSorteio);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSorteio;
+    }
+}
+
---
+++ projJogoDados/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projJogoDados
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSorteio_Click(object sender, EventArgs e)
+        {
+            int num1 = random.Next(1, 7);
+
+            if(num1 == 1)
+            {
+                pictureBox1.Image = Properties.Resources.dado1;
+            } else if(num1 == 2){
+                pictureBox1.Image = Properties.Resources.dado2;
+            } else if(num1 == 3)
+            {
+                pictureBox1.Image = Properties.Resources.dado3;
+            } else if(num1 == 4)
+            {
+                pictureBox1.Image = Properties.Resources.dado4;
+            } else if(num1 == 5)
+            {
+                pictureBox1.Image = Properties.Resources.dado5;
+            } else
+            {
+                pictureBox1.Image = Properties.Resources.dado6;
+            }
+        }
+    }
+}
